135cf0be56b7b91738aa2b5225ab0f2db3f3e6c4
jungmin <kimjm0825@gmail.com>
2022-06-16 19:05:09 +0900

    뽀모도로 타이머 레이아웃, SeekBar 코드 작성

---
+++ PomodoroTimer_part2_chapter06/app/src/main/java/com/example/pomodorotimer_part2_chapter06/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.pomodorotimer_part2_chapter06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    remainMinutesTextView.text = "%02d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar ?: return   // null 이면 진행하지 않음
+
+                    createCountDownTimer(seekBar.progress * 60 * 1000L).start()
+                }
+            }
+        )
+    }
+
+    private fun createCountDownTimer(initialMillis: Long): CountDownTimer {
+        return object : CountDownTimer(initialMillis, 1000L) {
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                updateRemainTime(0)
+                updateSeekBar(0)
+            }
+        }
+    }
+
+    private fun updateRemainTime(remainMillis: Long) {
+        val remainSeconds = remainMillis / 1000
+
+        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
+    }
+}
