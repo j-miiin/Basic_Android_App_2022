decb1a305ac35130417ec67d8964a69524502e51
jungmin <kimjm0825@gmail.com>
2022-06-17 18:38:07 +0900

    뽀모도로 타이머 완성

--- PomodoroTimer_part2_chapter06/app/src/main/java/com/example/pomodorotimer_part2_chapter06/MainActivity.kt
+++ PomodoroTimer_part2_chapter06/app/src/main/java/com/example/pomodorotimer_part2_chapter06/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pomodorotimer_part2_chapter06
 
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -20,30 +21,66 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.seekBar)
     }
 
+    private val soundPool = SoundPool.Builder().build()
+
+    private var currentCountDownTimer: CountDownTimer? = null
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bindViews()
+        initSounds()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        soundPool.autoResume()  // auto는 모든 stream을 재생, 정지
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundPool.autoPause()   // 화면을 껐을 때 소리도 멈추도록
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release() // 사용하지 않을 때는 해제해주는 것이 좋음
     }
 
     private fun bindViews() {
         seekBar.setOnSeekBarChangeListener(
             object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                    remainMinutesTextView.text = "%02d".format(progress)
+                    if (fromUser) { // 사용자가 조작했을 때만
+                        updateRemainTime(progress * 60 * 1000L)
+                    }
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    stopCountDown()
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
                     seekBar ?: return   // null 이면 진행하지 않음
 
-                    createCountDownTimer(seekBar.progress * 60 * 1000L).start()
+
+                    if (seekBar.progress == 0) {
+                        stopCountDown()
+                    } else {
+                        startCountDown()
+                    }
                 }
             }
         )
+    }
+
+    private fun initSounds() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
     }
 
     private fun createCountDownTimer(initialMillis: Long): CountDownTimer {
@@ -54,16 +91,43 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                updateRemainTime(0)
-                updateSeekBar(0)
+                completeCountDown()
             }
+        }
+    }
+
+    private fun startCountDown() {
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L).start()
+        currentCountDownTimer?.start()
+
+        // 1F : 사운드 좌우 볼륨, 1F가 최대
+        // -1 : loop forever
+        // rate : 재생 속도
+        tickingSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+        }
+    }
+
+    private fun stopCountDown() {
+        currentCountDownTimer?.cancel() // 현재 타이머를 멈춤
+        currentCountDownTimer = null
+        soundPool.autoPause()
+    }
+
+    private fun completeCountDown() {
+        updateRemainTime(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, 0, 1F)
         }
     }
 
     private fun updateRemainTime(remainMillis: Long) {
         val remainSeconds = remainMillis / 1000
 
-        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainMinutesTextView.text = "%02d'".format(remainSeconds / 60)
         remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
     }
 
