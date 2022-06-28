1a86047b32d19513735ed4e94ed9a72ec75f0b60
jungmin <kimjm0825@gmail.com>
2022-06-28 18:19:48 +0900

    녹음기 레이아웃 구현

---
+++ Recorder_part2_chapter07/app/src/main/java/com/example/recorder_part2_chapter07/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.recorder_part2_chapter07
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
+
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var state = State.BEFORE_RECORDING
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestAudioPermission()
+        initViews()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        // 권한 결과로 권한이 거부되었으면 앱을 종료
+        if (!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initViews() {
+        recordButton.updateIconWithState(state)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
+    }
+}
---
+++ Recorder_part2_chapter07/app/src/main/java/com/example/recorder_part2_chapter07/State.kt
@@ -0,0 +1,8 @@
+package com.example.recorder_part2_chapter07
+
+enum class State {
+    BEFORE_RECORDING,
+    ON_RECORDING,
+    AFTER_RECORDING,
+    ON_PLAYING
+}
